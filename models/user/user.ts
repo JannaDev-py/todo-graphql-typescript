4545210af846f973053cb6292c00aa4e11f9c836
Jannael Orlando <orlandojannael@gmail.com>
2025-07-21 21:19:38 -0600

    create user function with database and test

--- models/user/user.ts
+++ models/user/user.ts
@@ -1,9 +1,14 @@
-import { User } from '../../interfaces/user'
-import { Connection } from 'mongoose'
+import { CreateUser, User } from '../../interfaces/user'
+import UserDBModel from '../../database/schemas/user'
 
 const model = {
-  createUser: async function (args: User, connection: Connection): Promise<boolean> {
-    return true
+  createUser: async function (args: CreateUser): Promise<User | boolean> {
+    try {
+      const newUser = new UserDBModel(args)
+      return await newUser.save() as User
+    } catch (e) {
+      return false
+    }
   }
 }
 
---
+++ test/models/user/user.test.ts
@@ -0,0 +1,26 @@
+import UserModel from '../../../models/user/user'
+import mongoose from 'mongoose'
+
+beforeAll(async () => {
+  await mongoose.connect('mongodb://127.0.0.1:27017/testDB')
+})
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase()
+  await mongoose.connection.close()
+})
+
+describe('model-user', () => {
+  test('create user fn', async () => {
+    const response = await UserModel.createUser({ name: 'test', email: 'test', pwd: 'test' })
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        _id: expect.anything(),
+        name: 'test',
+        email: 'test',
+        pwd: 'test'
+      })
+    )
+  })
+})
